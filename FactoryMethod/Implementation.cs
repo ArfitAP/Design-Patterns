f3037ad3cfe3f4f866ceefd1104480a4f5a9e431
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-01 17:37:15 +0100

    Factory method

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,84 @@
 ﻿namespace FactoryMethod
 {
-   
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+
+        public override string ToString () => GetType().Name;
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get 
+            {
+                switch(_countryIdentifier) 
+                {
+                    case "BE":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService createDiscountService();
+    }
+
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService createDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService createDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
 }
+
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+var factories = new List<DiscountFactory>
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("BE")
+};
+
+foreach (var fact in factories)
+{
+    var discountService = fact.createDiscountService();
+    Console.WriteLine($"Percentage {discountService.DiscountPercentage} comming from {discountService}");
+}
+
+Console.ReadLine();
