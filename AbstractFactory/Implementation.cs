b6a444de5db481ef45d2d9460ee4997a4d7e0a8d
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-02 20:10:35 +0100

    Abstract factory

--- AbstractFactory/Implementation.cs
+++ AbstractFactory/Implementation.cs
@@ -1,4 +1,85 @@
 ﻿namespace AbstractFactory
 {
-   
+    public interface IShoppingCartPurchaseFactory
+    {
+        IDiscountService CreateDiscountService();
+
+        IShippingCostService CreateShippingCostService();
+    }
+
+    public interface IDiscountService
+    {
+        int DiscountPercentage { get; }
+    }
+
+    public class BelgiumDiscountService : IDiscountService
+    {
+        public int DiscountPercentage => 20;
+    }
+
+    public class FranceDiscountService : IDiscountService
+    {
+        public int DiscountPercentage => 10;
+    }
+
+    public interface IShippingCostService
+    {
+        decimal ShippingCost { get; }
+    }
+
+    public class BelgiumShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCost => 20;
+    }
+
+    public class FranceShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCost => 25;
+    }
+
+    public class BelgiumShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new BelgiumDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new BelgiumShippingCostService();
+        }
+    }
+
+    public class FranceShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new FranceDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new FranceShippingCostService();
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IShippingCostService _shippingCostService;
+        private int _orderCosts;
+
+        public ShoppingCart(IShoppingCartPurchaseFactory factory)
+        {
+            _discountService = factory.CreateDiscountService();
+            _shippingCostService = factory.CreateShippingCostService();
+
+            _orderCosts = 200;
+        }
+
+        public void CalculateCosts()
+        {
+            Console.WriteLine($"Total costs = {_orderCosts - (_orderCosts / 100 * _discountService.DiscountPercentage) + _shippingCostService.ShippingCost}");
+        }
+    }
 }
