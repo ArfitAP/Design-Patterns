4a5e99ebe6cd8c1de31f4131c974276bb31aa191
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-07 20:50:22 +0100

    Builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+//Console.WriteLine(miniBuilder.Car.ToString());
+garage.show();
+
+garage.Construct(bmwBuilder);
+//Console.WriteLine(bmwBuilder.Car.ToString());
+garage.show();
+
+Console.ReadLine();
