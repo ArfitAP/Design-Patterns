21c5c5b432eae71117874930c2e2c4499da05c4d
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-08 18:27:22 +0100

    Prototype

--- Prototype/Implementation.cs
+++ Prototype/Implementation.cs
@@ -1,4 +1,55 @@
-﻿namespace Prototype
-{
+﻿using Newtonsoft.Json;
 
+namespace Prototype
+{
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+
+        public abstract Person Clone(bool deepClone = false);
+    }
+
+    public class Manager : Person
+    {
+        public override string Name {  get; set; }
+
+        public Manager(string name) 
+        {
+            Name = name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Manager>(objectAsJson)!;
+            }
+
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public override string Name { get; set; }
+        public Manager Manager { get; set; }
+
+        public Employee(string name, Manager manager)
+        {
+            Name = name;
+            Manager = manager;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if(deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson)!;
+            }
+
+            return (Person)MemberwiseClone();
+        }
+    }
 }
