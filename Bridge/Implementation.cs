79d60f9392ad57799f4c5848baf45cef3fce9368
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-16 20:51:16 +0100

    Bridge

--- Bridge/Implementation.cs
+++ Bridge/Implementation.cs
@@ -1,3 +1,56 @@
 ﻿namespace Bridge
 {
+
+    public abstract class Menu
+    {
+        public readonly ICoupon _coupon = null!;
+        public abstract int CalculatePrice();
+
+        public Menu(ICoupon coupon)
+        {
+            _coupon = coupon;
+        }
+    }
+
+    public class VegeterianMenu : Menu
+    {
+        public VegeterianMenu(ICoupon coupon) : base(coupon) { }
+
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+
+    public class MeatBasedMenu : Menu
+    {
+        public MeatBasedMenu(ICoupon coupon) : base(coupon) { }
+
+        public override int CalculatePrice()
+        {
+            return 30 - _coupon.CouponValue;
+        }
+    }
+
+    public interface ICoupon
+    {
+        int CouponValue { get; }
+    }
+
+    public class NoCoupon : ICoupon
+    {
+        public int CouponValue { get => 0; }
+    }
+
+    public class OneEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 1; }
+    }
+
+    public class TwoEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 2; }
+    }
+
+
 }
--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Bridge";
+﻿using Bridge;
+
+Console.Title = "Bridge";
+
+var noCoupon = new NoCoupon();
+var oneEuroCoupon = new OneEuroCoupon();
+
+var meatBasedMenu = new MeatBasedMenu(noCoupon);
+Console.WriteLine($"Meat based menu, no coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+meatBasedMenu = new MeatBasedMenu(oneEuroCoupon);
+Console.WriteLine($"Meat based menu, one euro coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+var vegetarianMenu = new VegeterianMenu(noCoupon);
+Console.WriteLine($"Vegeterian menu, no coupon: {vegetarianMenu.CalculatePrice()} euro.");
+
+vegetarianMenu = new VegeterianMenu(oneEuroCoupon);
+Console.WriteLine($"Vegeterian menu, one euro coupon: {vegetarianMenu.CalculatePrice()} euro.");
+
+Console.ReadLine();
