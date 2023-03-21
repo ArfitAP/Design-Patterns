84e8b9fa2374a7561c914ad0ee00c73acef4f9b0
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-21 20:34:23 +0100

    Proxy, Flyweight

--- Flyweight/Implementation.cs
+++ Flyweight/Implementation.cs
@@ -1,3 +1,90 @@
 ﻿namespace Flyweight
 {
+    public interface ICharacter
+    {
+        void Draw(string fontFamily, int fontSize);
+    }
+
+    public class CharacterA : ICharacter
+    {
+        private char _actualCharacter = 'a';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+        }
+    }
+
+    public class CharacterB : ICharacter
+    {
+        private char _actualCharacter = 'b';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+        }
+    }
+
+    public class CharacterFactory
+    {
+        private readonly Dictionary<char, ICharacter> _characters = new();
+
+        public ICharacter? GetCharacter(char character) 
+        {
+            if( _characters.ContainsKey( character ) ) 
+            {
+                Console.WriteLine("Character reuse");
+                return _characters[ character ];
+            }
+            else
+            {
+                Console.WriteLine("Character construction");
+                switch( character ) 
+                {
+                    case 'a':
+                        _characters[character] = new CharacterA(); 
+                        return _characters[ character ];
+                    case 'b':
+                        _characters[character] = new CharacterB();
+                        return _characters[character];
+                }
+            }
+
+            return null;
+        }
+
+        public ICharacter CreateParagraph(List<ICharacter> characters, int location) 
+        {
+            return new Paragraph(location, characters);
+        }
+    }
+
+    public class Paragraph : ICharacter
+    {
+        private int _location;
+        private List<ICharacter> _characters = new();
+
+        public Paragraph(int location, List<ICharacter> characters)
+        {
+            _location = location;
+            _characters = characters;
+        }
+
+        public void Draw(string fontFamily, int fontSize)
+        {
+            Console.WriteLine($"Drawing in paragraph at location {_location}");
+            foreach(var character in _characters)
+            {
+                character.Draw(fontFamily, fontSize);
+            }
+        }
+    }
 }
--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Flyweight";
+﻿using Flyweight;
+
+Console.Title = "Flyweight";
+
+var bunchOfCharacters = "abba";
+
+var characterFactory = new CharacterFactory();
+
+var characterObject = characterFactory.GetCharacter(bunchOfCharacters[0]);
+characterObject?.Draw("Arial", 12);
+
+characterObject = characterFactory.GetCharacter(bunchOfCharacters[1]);
+characterObject?.Draw("Times New Roman", 14);
+
+characterObject = characterFactory.GetCharacter(bunchOfCharacters[2]);
+characterObject?.Draw("Comic Sans", 16);
+
+characterObject = characterFactory.GetCharacter(bunchOfCharacters[3]);
+characterObject?.Draw("Noto Sans", 10);
+
+
+// Create unshared concrete flyweight (paragraph)
+var paragraph = characterFactory.CreateParagraph(new List<ICharacter>() { characterObject }, 1);
+
+paragraph.Draw("Lucinda", 19);
+
+Console.ReadLine();
