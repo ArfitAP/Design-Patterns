511d9b9aef17306135584414aec183deda73981c
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-03-23 20:58:10 +0100

    Command

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.Title = "Command";
+﻿using Command;
+
+Console.Title = "Command";
+
+CommandManager commandManager = new CommandManager();
+IEmployeeManagerRepository repository = new EmployeeManagerRepository();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(111, "Kevin")));
+repository.WriteDataStore();
+
+commandManager.Undo();
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(222, "Clara")));
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+commandManager.UndoAll();
+repository.WriteDataStore();
+
+Console.ReadLine();
