3740b0e980ef18e16c0cad69c12e06b809963fa8
AntonioPavlis <antonio.pavlis51@gmail.com>
2023-04-03 20:10:54 +0200

    Chain of responsibility

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+var validDocument = new Document("How to avoid Java development", DateTime.Now, true, true);
+
+var invalidDocument = new Document("How to avoid Java development", DateTime.Now, false, true);
+
+var documentHandlerChain = new DocumentTitleHandler();
+documentHandlerChain.SetSuccessor(new DocumentLastModifiedHandler())
+                    .SetSuccessor(new DocumentApprovedByLitigationHandler())
+                    .SetSuccessor(new DocumentApprovedByManagementHandler());
+
+try 
+{
+    documentHandlerChain.Handle(validDocument);
+    Console.WriteLine("Valid document is valid");
+    documentHandlerChain.Handle(invalidDocument);
+    Console.WriteLine("Invalid document is valid");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
